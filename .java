03786955de59968ba31e194e6b0c3301ae609b0f
X Z <42951325+zhu415@users.noreply.github.com>
2025-09-02 00:19:06 -0400

    Update .java

--- .java
+++ .java
@@ -31,3 +31,40 @@ public void publishExtraOutputs(ExtraOutputCollector extraOutputsCollector,
     final String COST = "IndexFeeReport";
     // ... continue with existing publishExtraOutputs code ...
 }
+
+
+public void publishExtraOutputs(ExtraOutputCollector extraOutputsCollector, 
+        PortfolioContextRepository portfolioRepository, 
+        IndexSpecificDailyResult todaysDailyResult) throws AMGException {
+    
+    // Print weights to console
+    System.out.println("========== WEIGHTS OUTPUT ==========");
+    System.out.println("publishExtraOutputs called");
+    
+    if (todaysDailyResult.myWeights != null) {
+        System.out.println("Weights array length: " + todaysDailyResult.myWeights.length);
+        for (int i = 0; i < todaysDailyResult.myWeights.length; i++) {
+            if (i < myIdentifier.size()) {
+                System.out.println("Weight[" + myIdentifier.get(i).getMarketId() + "]: " + todaysDailyResult.myWeights[i]);
+            } else {
+                System.out.println("Weight[" + i + "]: " + todaysDailyResult.myWeights[i]);
+            }
+        }
+    } else {
+        System.out.println("myWeights is NULL");
+    }
+    
+    if (myThirdPartyIndexComps != null) {
+        System.out.println("ThirdPartyIndexComps:");
+        for (Map.Entry<String, Double> entry : myThirdPartyIndexComps.entrySet()) {
+            System.out.println("  " + entry.getKey() + ": " + entry.getValue());
+        }
+    }
+    
+    System.out.println("Index Price: " + todaysDailyResult.myPrice[0]);
+    System.out.println("=====================================");
+    
+    // Your existing code continues...
+    final String COST = "IndexFeeReport";
+    // ... rest of existing code ...
+}
